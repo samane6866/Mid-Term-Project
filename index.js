7fd4401fd3ac69e44d0e63eb7a6f3ee94aa5cc05
your_github_username <your_github_email>
2022-09-23 13:58:33 +0200

    modified css

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const hambuger = document.querySelector(".hamburger-btn");
+const navbar = document.querySelector(".nav-bar");
 
 hambuger.addEventListener("click", () => {
   hambuger.classList.toggle("active");
+  navbar.classList.toggle("active");
 });
